3240b1cdc1a627aa5cabf6198005de2a621fc525
thiagocamerato757 <cameratothiago@gmail.com>
2024-11-06 21:19:02 -0300

    criação de template de perguntas. TODO: fazer o gabarito.py

---
+++ TemplateTrabDoGrupo02.py
@@ -0,0 +1,26 @@
+"""
+    nome dos integrantes do grupo:
+    - Thiago Pereira Camerato - 2212580 33B
+    -
+    -
+    -
+"""
+
+print("\n-----------------------------------------------------")
+print("\n 1. Qual é o valor médio de uma diária por tipo de quarto e bairro?")
+print("\n-----------------------------------------------------")
+print("\n-----------------------------------------------------")
+print("\n 2. Qual é a distribuição de avaliações dos hosts por número de propriedades?")
+print("\n-----------------------------------------------------")
+print("\n-----------------------------------------------------")
+print("\n 3. Quais bairros possuem a maior e menor disponibilidade média de dias no ano?")
+print("\n-----------------------------------------------------")
+print("\n-----------------------------------------------------")
+print("\n 4. Existe uma correlação entre a política de cancelamento e a taxa de avaliações mensais?")
+print("\n-----------------------------------------------------")
+print("\n-----------------------------------------------------")
+print("\n 5. Como o preço é distribuído entre os diferentes anos de construção das propriedades?")
+print("\n-----------------------------------------------------")
+print("\n-----------------------------------------------------")
+print("\n 6. Quais bairros possuem mais listagens com reserva instantânea?")
+print("\n-----------------------------------------------------")
